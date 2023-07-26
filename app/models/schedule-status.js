60cf0dd58891368c3323ad8d7ca15e288bf6d207
Eric Bai <eric@ericbai.com>
2023-07-26 19:49:34 -0400

    Added support for both interns and residents

--- app/models/schedule-status.js
+++ app/models/schedule-status.js
@@ -6,12 +6,14 @@ export default class ScheduleStatusModel extends Model {
   @attr('string') minDate;
   @attr('string') maxDate;
 
-  @attr('string') blockName;
-  @attr('number') dayNumber;
+  // Objects with keys `blockName`, `isA`, and `dayNumber`
+  @attr internBlockInfo;
+  @attr residentBlockInfo;
 
-  // Arrays of objects with keys `name` and `assignment`
+  // Arrays of objects with keys `name`, `role`, and `assignment`
   @attr off;
   @attr maybeOff;
+  @attr likelyNotOff;
 
   get readableDate() {
     return dayjs(this.fetchedDate).format('ddd, MMM D');
